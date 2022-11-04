5ab7da24441f59ecfcf6cd9dfc83b8ee2d13a6a6
silabs-WayneW <wangjinchao126@126.com>
2022-11-04 21:51:02 +0800

    Add Book and three atributes.

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public List<string> Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+}
